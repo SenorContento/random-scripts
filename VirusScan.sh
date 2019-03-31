aaceaa963c02539fa4a460d7a957a0d921b0420b
Alex Contento <github@senorcontento.com>
2019-03-31 16:32:34 -0400

    Added Successful Virus Scan Script

--- VirusScan.sh
+++ VirusScan.sh
@@ -5,9 +5,11 @@ mysql=/usr/bin/mysql
 cat=/bin/cat
 echo=echo #/bin/echo
 cd=cd
+tail=/usr/bin/tail
+nice=/usr/bin/nice
 
 mysqlAuth=/home/alex/mysql_auth/maldet-password.cnf
-database="piet"
+database="$2"
 
 uploadDirectory="/var/web/term-uploads/"
 niceValue=19
@@ -17,10 +19,18 @@ programid="$1"
 $cd $($dirname "$0") # Changes to Program Directory
 
 $echo "Scanning For Viruses"
-$maldet --scan-all $uploadDirectory"piet_"$programid".png";
+#$echo "$maldet --scan-all $uploadDirectory piet_ $programid .png && echo passed || echo failed"
+maldet_output=$($nice -n $niceValue $maldet --scan-all $uploadDirectory"piet_"$programid".png" && $echo "Passed" || $echo "Failed")
+#exitcode="$?"
+$echo "Finished Scan!!!"
+
+$echo -e "$maldet_output"
+maldet_output=$($echo -e "$maldet_output" | $tail -n 1)
 
 # https://stackoverflow.com/a/8230301/6828099
-if [ "$?" != "0" ]; then
+#$echo "Exit Code: \"$exitcode\"";
+$echo "Maldet Output: $maldet_output"
+if [ "$maldet_output" == "Failed" ]; then
   $echo "Failed Virus Scan!!!"
   query="UPDATE programs SET allowed='0', banreason='Failed Virus Scan' WHERE programid='$programid';"
 
